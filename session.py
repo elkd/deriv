82e56cbad5148a8064f9df0a706b7bcf492b9861
elkd <el@obrisk.com>
2022-03-14 21:51:07 +0800

    implemented the initial version of Deriv trader bot

---
+++ session.py
@@ -0,0 +1,119 @@
+from playwright.sync_api import Playwright, sync_playwright
+from prob import check_stop
+
+
+class TradeSession:
+    def __init__(self):
+        self.mtng = 0.01
+        self.init_stake = 0.35
+        self.stop_loss = 2
+        self.stop_profit = 2
+        self.loop = False
+
+        with sync_playwright() as playwright:
+            self.browser = playwright.chromium.launch(headless=False)
+            self.context = self.browser.new_context()
+
+            # Open new page
+            self.page = self.context.new_page()
+
+
+    def login(email, psword):
+        # Go to https://smarttrader.deriv.com/
+        self.page.goto("https://smarttrader.deriv.com/")
+
+        # Click text=Log in
+        lg_btn = self.page.locator("#btn__login") or self.page.locator("text=Log in")
+        lg_btn.click()
+
+        self.page.wait_for_timeout(9000)
+
+        # Click [placeholder="example\@email\.com"]
+        email_input = self.page.locator('#txtEmail') or self.page.locator("[placeholder=\"example\\@email\\.com\"]")
+        email_input.fill(email)
+
+        self.page.wait_for_timeout(11000)
+
+        psword_input = self.page.locator('#txtPass') or self.page.locator("input[name=\"password\"]")
+
+        # Fill input[name="password"]
+        psword_input.fill(psword)
+
+        self.page.wait_for_timeout(9000)
+        # Click button:has-text("Log in")
+        # with page.expect_navigation(url="https://smarttrader.deriv.com/en/trading.html"):
+        with self.page.expect_navigation():
+            submit_btn = self.page.locator('button.button.secondary') or self.page.locator('input[name="login"]')
+            submit_btn.click()
+            #page.locator("button:has-text(\"Log in\")").click()
+        # assert page.url == "https://smarttrader.deriv.com/en/logged_inws.html?acct1=VRTC5668415&token1=a1-5YB08Egh7wehF3DMq5ihyFDSRNnZK&cur1=USD&state="
+
+
+    def play(self, window, values):
+        print(values['_SK_'])
+        if not values['_SK_'] or not values['MG'] or not values['_LDP_']:
+            window['_MESSAGE_'].update('Please provide LDP, Stake and initial Martingale')
+            return
+
+        self.loop = True
+        self.mtng = values['_MG_']
+        self.stop_loss = values['_SL_']
+        self.stop_profit = values['_SP_']
+        stake = values['_SK_']
+        ldp = values['_LDP_']
+
+        start_balance = self.page.locator("#header__acc-balance").inner_text().split()[0]
+        window['_START_BAL_'].update(start_balance)
+
+        while self.loop:
+            self.page.wait_for_timeout(8000)
+            self.page.goto(
+                f"https://smarttrader.deriv.com/en/trading.html?currency=USD&market=synthetic_index&underlying=R_100&formname=matchdiff&date_start=now&duration_amount=1&duration_units=t&amount={stake}&amount_type=stake&expiry_type=duration&prediction={ldp}"
+            )
+
+            self.page.wait_for_timeout(13000)
+            #10,001.83 USD
+            balance = self.page.locator("#header__acc-balance").inner_text().split()[0]
+            window['_CURRENT_BAL_'].update(balance)
+
+            # Click #purchase_button_top
+            self.page.locator("#purchase_button_top").click()
+
+            self.page.wait_for_timeout(21000)
+            # Click text=This contract lost
+            result_str = self.page.locator("#contract_purchase_heading").inner_text()
+            print(result_str)
+
+            win = True
+            if result_str == "This contract lost":
+                win = False
+
+            self.page.wait_for_timeout(7000)
+            # Click #close_confirmation_container
+            self.page.locator("#close_confirmation_container").click()
+
+            martingale, stop_est = check_stop(self, start_balance, stake)
+            window['_STOP_EST_'].update(stop_est)
+
+            if not martingale:
+                self.loop = False
+                break
+
+            if win:
+                stake = self.init_stake
+            else:
+                stake = round(stake+martingale, 2)
+
+
+    def pause(self):
+        self.loop = False
+
+
+    def stop(self):
+        self.loop = False
+
+
+    def exit(self):
+        self.context.close()
+        self.browser.close()
+        return
---
+++ trade.py
@@ -0,0 +1,68 @@
+import secrets
+import PySimpleGUI as sg
+from session import TradeSession
+
+
+def btn(name, key=secrets.token_urlsafe()):
+    '''
+    # a PySimpleGUI "User Defined Element" (see docs)
+    '''
+    return sg.Button(name, key=key, size=(6, 1), pad=(1, 1))
+
+
+if __name__ == '__main__':
+    sg.theme('BluePurple')
+
+    layout = [
+        [sg.Text(size=(15,1), key='_MESSAGE_')],
+
+        [sg.Text('Email'), sg.Input(size=(12,1), k='_EMAIL_'), sg.Text(size=(7,1)), sg.Text('Password'), sg.Input(size=(12,1), k='_PWORD_')],
+
+        [btn('Login', '_LOGIN_')],
+
+        [sg.Text('Start Balance:'), sg.Text(size=(8,1), key='_START_BAL_'),
+            sg.Text('Current Balance:'), sg.Text(size=(12,1), key='_CURRENT_BAL_'),
+                sg.Text('Current Loss/Profit:'), sg.Text('0.00', size=(12,1), key='_STOP_EST_')],
+
+
+        [sg.Text('Stop Loss:'), sg.Input(size=(7,1), k='_SL_'),
+            sg.Text('Stop Profit:'), sg.Input(size=(7,1), k='_SP_'), sg.Text('Stake:'), sg.Input(size=(7,1), k='_SK_'),
+                sg.Text('Martingale:'), sg.Input(size=(7,1), k='_MG_'), sg.Text('LDP:'), sg.Input(size=(7,1), k='_LDP_')],
+
+        [btn('Play ▶️', '_BUTTON_PLAY_'), btn('Pause ⏸️', '_BUTTON_PAUSE_'), btn('Stop ⏹️', '_BUTTON_STOP_')],
+
+    ]
+
+    window = sg.Window('Deriv SmartTrader Bot', layout, element_justification='center', finalize=True, resizable=True)
+
+    #list_player = inst.media_list_player_new()
+    #media_list = inst.media_list_new([])
+    #list_player.set_media_list(media_list)
+    #player = list_player.get_media_player()
+
+    trade_session = TradeSession()
+
+    while True:  # Event Loop
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            trade_session.exit()
+            break
+
+        if event == '_LOGIN_':
+            if values['_EMAIL_'] and values['_PWORD_']:
+                trade_session.login(values['_EMAIL_'], values['_PWORD_'])
+            else:
+                window['_MESSAGE_'].update('Please provide Email and Password')
+
+        if event == '_BUTTON_PLAY_':
+            trade_session.play(
+                    window, values
+                )
+        if event == '_BUTTON_PAUSE_':
+            trade_session.pause()
+
+        if event == '_BUTTON_STOP_':
+            trade_session.stop()
+
+    window.close()
